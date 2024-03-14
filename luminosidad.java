6cb4f115805386c903841fce31963500672c910d
LorenzoGarciaH <159237169+LorenzoGarciaH@users.noreply.github.com>
2024-03-14 11:24:47 +0000

    primeros cambios

---
+++ luminosidad.java
@@ -0,0 +1,3 @@
+public enum luminosidad {
+    ALTA, MEDIA, BAJA;
+}
---
+++ poblacion.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class poblacion {
+    // Atributos de la clase Poblacion
+    private String nombre;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private int cantidad;
+    private luminosidad luminosidad;
+    private Dosis dosisComida;
+
+    // Constructor de la clase Poblacion
+    public poblacion(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.fechaInicio = new Date();
+        this.fechaFin = new Date();
+    }
+
+    // Métodos getter y setter para los atributos
+    public String getNombre() {
+        return nombre;
+    }
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public luminosidad getLuminosidad() {
+        return luminosidad;
+    }
+
+    public void setLuminosidad(luminosidad luminosidad) {
+        this.luminosidad = luminosidad;
+    }
+
+    public Dosis getDosis() {
+        return dosisComida;
+    }
+
+    public void setDosis(Dosis dosis) {
+        this.dosisComida = dosis;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    // Método toString para representar la clase como una cadena de texto
+    @Override
+    public String toString() {
+        return "Poblacion{" +
+                "nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                '}';
+    }
+}
